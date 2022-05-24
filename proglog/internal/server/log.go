890c036f23f51eae6d17d0543aaec66ae81ee6b9
root <root@DESKTOP-DIRN7TL.localdomain>
2022-05-24 11:32:39 -0400

    adding more code and some temporary? comments for my understanding

--- proglog/internal/server/log.go
+++ proglog/internal/server/log.go
@@ -21,13 +21,32 @@ func NewLog() *Log {
 // var c *Log
 // c.Append()
 func (c *Log) Append(record Record) (uint64, error) {
+	// Lock the mutex for the life of the function and defer an unlock for after execution 
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	// Set the record's offset to the current position in the ledger 
+	record.Offset = uint64(len(c.records))
+	// Append the current record. Append returns a copy
+	c.records = append(c.records, record)
+	// Return the records offset and nil
+	return record.Offset, nil
+}
 
+func (c *Log) Read (offset uint64) (Record, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if offset >= uint64(len(c.records)) {
+		return Record{}, ErrOffsetNotFound
+	}
+	return c.records[offset], nil
 }
 
 
 
 type Record struct {
 	Value []byte `json:"value"`
+	Offset uint64 `json:"offset`
 }
 
 var ErrOffsetNotFound = fmt.Errorf("offset not found")
